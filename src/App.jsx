5b397510ff24d8aa6d43e57d5028f5bb5859bca5
Paulo Xavier <paulohenriquex18@gmail.com>
2026-04-25 16:12:01 -0300

    feat(app): add Lenis for smooth scrolling integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,36 @@ import ContactForm from "./pages/ContactForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import RootLayout from "./RootLayout";
+import { ReactLenis } from "lenis/react";
+import { cancelFrame, frame } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    function update(data) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+    frame.update(update, true);
+    return () => {
+      cancelFrame(update);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<ContactForm />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<ContactForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ReactLenis>
   );
 }
 
