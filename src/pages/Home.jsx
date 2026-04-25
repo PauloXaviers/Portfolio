e62f104c91e8027e3e9a83918426b37e8295e337
Paulo Xavier <paulohenriquex18@gmail.com>
2026-04-25 16:12:52 -0300

    refactor(home): remove unused StaggerChildren component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import StaggerChildren from "../components/StaggerChildren"
-
 
 const Home = () => {
     return(
     <section>
-    
         <div className="mt-10">
             <h1 className="text-amber-600">
                 Voce esta na home
@@ -16,11 +13,7 @@ const Home = () => {
             <div className="h-[200px]">Teste teste teste</div>
             <div className="h-[200px]">Teste teste teste</div>
             <div className="h-[200px]">Teste teste teste</div>
-            
-        
         </div>
-
-        <StaggerChildren />
     </section>
     )
 }
