03bd0ff1d76723bf8fa578e1afb940588e9a9ea4
Pauloxaviers <paulohenriquex18@gmail.com>
2026-05-31 21:55:56 -0300

    feat(projects): add data and content for projects section (WIP)

--- src/components/modal/ModalContainer.jsx
+++ src/components/modal/ModalContainer.jsx
@@ -17,7 +17,7 @@ const ModalContainer = ({ children }) => {
       initial="hidden"
       animate="visible"
       exit="exit"
-      className="fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-50 w-[90vw] md:w-[50vw] bg-[#080808f5] rounded-2xl shadow-[0px_0px_80px_30px_#000] border border-(--cor-branca) p-10 md:p-10 flex flex-col justify-center items-center"
+      className="fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-50 w-[90vw] md:w-[50vw] max-h-[80vh] bg-[#080808f5] rounded-2xl shadow-[0px_0px_80px_30px_#000] border border-(--cor-branca) p-10 md:p-10 flex flex-col justify-start items-center gap-5 overflow-y-auto"
     >
       {children}
     </motion.div>
--- src/components/projects/ProjectSection.jsx
+++ src/components/projects/ProjectSection.jsx
@@ -1,29 +1,44 @@
-import { detailsProjects, cardsProjects } from "../../contents/projectsData.js";
+import { projectDetails, cardsProjects } from "../../contents/projectsData.js";
 import { motion } from "motion/react";
 import { useState } from "react";
 import Card from "../card/index.jsx";
 import ActionLink from "../ActionLink.jsx";
-
+import { Modal } from "../modal/index.jsx";
 
 const variantsTitle = {
   hidden: { x: -100, opacity: 0 },
   visible: {
     x: 0,
     opacity: 1,
-    transition: { duration: 0.5, delay: 0.3, type: "spring", stiffness: 70, damping: 15 },
+    transition: {
+      duration: 0.5,
+      delay: 0.3,
+      type: "spring",
+      stiffness: 70,
+      damping: 15,
+    },
   },
 };
 
 const ProjectSection = ({ variant = "homePage" }) => {
-  const contentList = variant === "homePage" ? cardsProjects.slice(0, 2) : cardsProjects.length;
-  const { title } = detailsProjects;
-  const [{ text }, { text: projectText }] = detailsProjects.description;
+  const contentList =
+    variant === "homePage" ? cardsProjects.slice(0, 2) : cardsProjects.length;
+  const { title } = projectDetails;
+  const [{ text }, { text: projectText }] = projectDetails.description;
   const isHome = variant === "homePage";
-  // const [modal, setModal] = useState(false)
-  
-  // const toggleModal = () =>{
-  //   setModal(!modal)
-  // }
+  const [modal, setModal] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleCloseModal = () => {
+    setModal(false);
+    setSelectedProject(null);
+  };
+
+  const handleOpenModal = (project) => {
+    setModal(true);
+    setSelectedProject(project);
+  };
+
   const cardClass = {
     home: "flex flex-col gap-5 items-center justify-center w-full",
     project: "flex flex-col md:flex-row gap-5 items-center justify-center ",
@@ -38,16 +53,27 @@ const ProjectSection = ({ variant = "homePage" }) => {
         variants={variantsTitle}
       >
         <div className="w-full flex flex-col gap-3 mb-4">
-          {isHome ? ( <h2 className="text-2xl font-normal text-white">{title}</h2> ) : ( <h1 className="">{title}</h1> )}
-          <p className="text-white font-extralight text-justify">{isHome ? text : projectText}</p>
+          {isHome ? (
+            <h2 className="text-2xl font-normal text-white">{title}</h2>
+          ) : (
+            <h1 className="">{title}</h1>
+          )}
+          <p className="text-white font-extralight text-justify">
+            {isHome ? text : projectText}
+          </p>
         </div>
       </motion.div>
       <section
         className={`w-full items-center justify-center gap-7 ${isHome ? "flex flex-col lg:flex-row " : "flex flex-col"}`}
       >
         {contentList.map((item) => (
-          <Card.Container key={item.id} className={`flex justify-center items-start rounded-2xl ${ isHome ? "w-full lg:w-[40vw] min-h-90 h-auto md:h-155" : "w-full" }`} >
-            <Card.Content className={`${isHome ? cardClass.home : cardClass.project}`}>
+          <Card.Container
+            key={item.id}
+            className={`flex justify-center items-start rounded-2xl ${isHome ? "w-full lg:w-[40vw] min-h-90 h-auto" : "w-full"}`}
+          >
+            <Card.Content
+              className={`${isHome ? cardClass.home : cardClass.project}`}
+            >
               <img src={item.imgUrl} className="rounded-2xl w-full" />
               <div className="flex flex-col w-full px-5 gap-3 pb-5">
                 <h3 className="text-white text-xl">{item.name}</h3>
@@ -59,32 +85,61 @@ const ProjectSection = ({ variant = "homePage" }) => {
                       key={tech.name}
                       className="card-container container-shadow flex flex-row gap-2 items-center justify-center rounded-md py-1 px-2"
                     >
-                      <img src={tech.imgUrl} className="w-8" alt="Imagem do icone da técnologia" />
-                      <p className="text-white font-extralight text-[13px]">{tech.name}</p>
+                      <img
+                        src={tech.imgUrl}
+                        className="w-8"
+                        alt="Imagem do icone da técnologia"
+                      />
+                      <p className="text-white font-extralight text-[13px]">
+                        {tech.name}
+                      </p>
                     </div>
                   ))}
                 </div>
-                {/* <button 
-                  onClick={toggleModal}
+                <button
+                  onClick={() => handleOpenModal(item.videoData)}
                   className="card-container container-shadow text-white whitespace-nowrap py-2 px-1 flex flex-row gap-2 items-center justify-center rounded-md w-40 cursor-pointer"
                 >
                   Leia sobre
-                </button> */}
+                </button>
                 {item.projectUrl.map((link) => (
-                  <div key={link.name} className="flex flex-row gap-1 py-1 px-1 items-center">
-                    <ActionLink
-                      url={link.url}
-                      className="card-container whitespace-nowrap py-1 px-1 flex flex-row gap-2 items-center justify-center rounded-md w-40"
-                    >
-                      <img src={link.imgUrl} alt={link.imgAlt} className="w-8" />
-                      {link.name}
-                    </ActionLink>
-                  </div>
+                  <ActionLink
+                    url={link.url}
+                    key={link.name}
+                    className="card-container whitespace-nowrap py-1 px-1 flex flex-row gap-2 items-center justify-center rounded-md w-40"
+                  >
+                    <img src={link.imgUrl} alt={link.imgAlt} className="w-8" />
+                    {link.name}
+                  </ActionLink>
                 ))}
               </div>
             </Card.Content>
           </Card.Container>
         ))}
+
+        {modal && (
+          <Modal.Container>
+            <Modal.Header className="h-13" modalClose={handleCloseModal}>
+              <h2 className="text-white text-3xl font-medium whitespace-nowrap">
+                {selectedProject.title}
+              </h2>
+            </Modal.Header>
+            <Modal.Contents className="flex flex-col gap-5 items-center justify-center">
+              <video autoPlay controls>
+                <source src={selectedProject.videoUrl} type="video/mp4" />
+              </video>
+              <p className="whitespace-pre-line text-white font-extralight text-[15px]">
+                {selectedProject.description}
+              </p>
+              <ActionLink
+                url={selectedProject.url}
+                className="whitespace-nowrap py-1 px-1 flex flex-row gap-2 items-center justify-center rounded-md w-40"
+              >
+                Ir para o site
+              </ActionLink>
+            </Modal.Contents>
+          </Modal.Container>
+        )}
       </section>
     </section>
   );
